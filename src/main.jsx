ec04426fe6375059ad3830353e0f6b7b7bfd5ec9
Khalilullah Baig <khalilullahbaig1999@gmail.com>
2026-07-30 00:34:55 +0500

    Fix GitHub Pages path

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import './index.css'   // 👈 yahan apni css import karo
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/EduPanel-Student-Portal-UI/">
+  <BrowserRouter basename="/EduPanel-/">
     <App />
   </BrowserRouter>
 );
